cd5fc91b6c398c5b616383b59bbe1b0355d29768
JacquelineJia <jackiesisy@gmail.com>
2019-09-25 09:43:51 -0400

    Updates

---
+++ precache-manifest.fbf0c242918603d7898200c5ab20ae9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a79d14642f2c305f3fd8ea473b70312",
+    "url": "/static/media/home-page-img.4a79d146.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7bfde91a3d33aaab23b2",
+    "url": "/static/js/main.7bfde91a.chunk.js"
+  },
+  {
+    "revision": "ed466804eeadf2e24550",
+    "url": "/static/js/1.ed466804.chunk.js"
+  },
+  {
+    "revision": "7bfde91a3d33aaab23b2",
+    "url": "/static/css/main.8e6a41af.chunk.css"
+  },
+  {
+    "revision": "86b88646997ee75907176142fdafe995",
+    "url": "/index.html"
+  }
+];
