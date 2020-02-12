a168b2166ae5f1270f112bbdfb8c8b0d116bdee9
JacquelineJia <jackiesisy@gmail.com>
2020-02-12 15:50:33 -0500

    Updates

---
+++ precache-manifest.ef599136ecbe92817d8fdc203510091a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a79d14642f2c305f3fd8ea473b70312",
+    "url": "/static/media/home-page-img.4a79d146.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c936d944edfcdbd62efa",
+    "url": "/static/js/main.c936d944.chunk.js"
+  },
+  {
+    "revision": "279a8a9e026fb0881a73",
+    "url": "/static/js/1.279a8a9e.chunk.js"
+  },
+  {
+    "revision": "c936d944edfcdbd62efa",
+    "url": "/static/css/main.ba940bd1.chunk.css"
+  },
+  {
+    "revision": "2ec3d88911c3389b186bf3b538ac2358",
+    "url": "/index.html"
+  }
+];
