27e13eea32db25b5d211415ce5f3988486441032
JacquelineJia <jackiesisy@gmail.com>
2020-02-12 15:43:11 -0500

    Updates

---
+++ precache-manifest.eea9e6c1fabaf54f52d987668b807c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oriental-dance-troupe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a79d14642f2c305f3fd8ea473b70312",
+    "url": "/oriental-dance-troupe/static/media/home-page-img.4a79d146.jpg"
+  },
+  {
+    "revision": "cac300d9193e299b1f3c",
+    "url": "/oriental-dance-troupe/static/js/runtime~main.cac300d9.js"
+  },
+  {
+    "revision": "49c994e7747364575bf3",
+    "url": "/oriental-dance-troupe/static/js/main.49c994e7.chunk.js"
+  },
+  {
+    "revision": "279a8a9e026fb0881a73",
+    "url": "/oriental-dance-troupe/static/js/1.279a8a9e.chunk.js"
+  },
+  {
+    "revision": "49c994e7747364575bf3",
+    "url": "/oriental-dance-troupe/static/css/main.af715cd7.chunk.css"
+  },
+  {
+    "revision": "353c89834019ac79c210341ca66f4b95",
+    "url": "/oriental-dance-troupe/index.html"
+  }
+];
