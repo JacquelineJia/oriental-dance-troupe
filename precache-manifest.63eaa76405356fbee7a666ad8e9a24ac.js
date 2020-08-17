aafcb244d1dbf020b4922a67a26aa6a6788f7f20
JacquelineJia <jackiesisy@gmail.com>
2020-08-16 22:58:06 -0400

    Updates

---
+++ precache-manifest.63eaa76405356fbee7a666ad8e9a24ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e5b0d49d9d60c23747d",
+    "url": "/static/css/main.de032e21.chunk.css"
+  },
+  {
+    "revision": "2e5b0d49d9d60c23747d",
+    "url": "/static/js/main.2e5b0d49.chunk.js"
+  },
+  {
+    "revision": "fcb901f970d00082cd31",
+    "url": "/static/js/1.fcb901f9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "985ec3749e73276deb1e4c2d76eff649",
+    "url": "/static/media/tao-ding.985ec374.jpg"
+  },
+  {
+    "revision": "4a79d14642f2c305f3fd8ea473b70312",
+    "url": "/static/media/home-page-img.4a79d146.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39c06066654e7ad8ff0a1c771fd20243",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d5abb47d1af6754514120f119fa00c3.js"
+  "/precache-manifest.63eaa76405356fbee7a666ad8e9a24ac.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
