1e28d4fc7c49e02dea2c2828e962a72ef29ff1e1
JacquelineJia <jackiesisy@gmail.com>
2020-08-16 22:48:32 -0400

    added team member cards

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -13,41 +13,61 @@ class Team extends Component {
           />
         </span>
         <span className={style.ourTeamContent}>
-          <span className={style.subtitle}>
-            <FormattedMessage
-              id="components.about.content.ourTeam.director.name"
-              defaultMessage="Tao Ding"
-            />
+          <span className={style.memberCard}>
+            <span className={style.column}>
+              <span className={style.row}>
+                <span className={style.name}>
+                  <FormattedMessage
+                    id="components.about.content.ourTeam.director.name"
+                    defaultMessage="Tao Ding"
+                  />
+                </span>
+                <span className={style.role}>
+                  <FormattedMessage
+                    id="components.about.content.ourTeam.director"
+                    defaultMessage="director"
+                  />
+                </span>
+              </span>
+              <div className={style.imageTaoDing}/>
+            </span>
+            <span className={style.description}>
+              <FormattedMessage
+                id="components.about.content.ourTeam.director.about"
+                defaultMessage=""
+                values={{
+                  linebreak: <br />
+                }}
+              />
+            </span>
           </span>
-          <span className={style.content}>
-            <FormattedMessage
-              id="components.about.content.ourTeam.director"
-              defaultMessage="director"
-            />
+          <span className={style.memberCard}>
+            <span className={style.name}>
+              <FormattedMessage
+                id="components.about.content.ourTeam.artisticDirector.name"
+                defaultMessage="Junwei Bao"
+              />
+            </span>
+            <span className={style.role}>
+              <FormattedMessage
+                id="components.about.content.ourTeam.artisticDirector"
+                defaultMessage="Artistic Director"
+              />
+            </span>
           </span>
-          <span className={style.subtitle}>
-            <FormattedMessage
-              id="components.about.content.ourTeam.artisticDirector.name"
-              defaultMessage="Junwei Bao"
-            />
-          </span>
-          <span className={style.content}>
-            <FormattedMessage
-              id="components.about.content.ourTeam.artisticDirector"
-              defaultMessage="Artistic Director"
-            />
-          </span>
-          <span className={style.subtitle}>
-            <FormattedMessage
-              id="components.about.content.ourTeam.instructor.name.1"
-              defaultMessage="Diana Ding"
-            />
-          </span>
-          <span className={style.content}>
-            <FormattedMessage
-              id="components.about.content.ourTeam.instructor"
-              defaultMessage="Instructor"
-            />
+          <span className={style.memberCard}>
+            <span className={style.name}>
+              <FormattedMessage
+                id="components.about.content.ourTeam.instructor.name.1"
+                defaultMessage="Diana Ding"
+              />
+            </span>
+            <span className={style.role}>
+              <FormattedMessage
+                id="components.about.content.ourTeam.instructor"
+                defaultMessage="Instructor"
+              />
+            </span>
           </span>
         </span>
       </div>
