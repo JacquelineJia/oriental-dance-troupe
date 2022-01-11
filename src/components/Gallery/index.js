dc6bd23596117eae9eadea9766ff1299c121c7ca
JacquelineJia <>
2022-01-10 22:17:27 -0500

    remove some videos from gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -15,42 +15,6 @@ class Gallery extends Component {
         <span className={style.videoGroup}>
           <span className={`${style.videoContainer} ${style.singleVideo}`}>
             <iframe
-              title="kids dance good"
-              src="https://www.youtube.com/embed/tNi7YxK_vw4"
-              allowfullscreen="allowfullscreen"
-            >
-            </iframe>
-          </span>
-        </span>
-        <span className={style.videoGroup}>
-          <span className={style.videoContainer}>
-            <iframe
-              title="Choreographed by bao 1"
-              src="https://www.youtube.com/embed/CqV3vmUUdno"
-              allowfullscreen="allowfullscreen"
-            >
-            </iframe>
-          </span>
-          <span className={style.videoContainer}>
-            <iframe
-              title="Choreographed by bao 2"
-              src="https://www.youtube.com/embed/MZ8exzP0Fwo"
-              allowfullscreen="allowfullscreen"
-            >
-            </iframe>
-          </span>
-          <span className={style.videoContainer}>
-            <iframe
-              title="Choreographed by bao 3"
-              src="https://www.youtube.com/embed/ONrJVQcSHks"
-              allowfullscreen="allowfullscreen"
-            >
-            </iframe>
-          </span>
-        </span>
-        <span className={style.videoGroup}>
-          <span className={`${style.videoContainer} ${style.singleVideo}`}>
-            <iframe
               title="30 years anniversary"
               src="https://www.youtube.com/embed/rP5xkDrV3P4"
               allowfullscreen="allowfullscreen"
@@ -59,14 +23,6 @@ class Gallery extends Component {
           </span>
         </span>
         <span className={style.videoGroup}>
-          <span className={style.videoContainer}>
-            <iframe
-              title="kids dance/song with good camera"
-              src="https://www.youtube.com/embed/WwY1PohyDU0"
-              allowfullscreen="allowfullscreen"
-            >
-            </iframe>
-          </span>
           <span className={style.videoContainer}>
             <iframe
               title="Dance cover 1"
